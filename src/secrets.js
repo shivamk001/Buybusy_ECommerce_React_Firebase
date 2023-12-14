92ed6c738048f0136aaa6ef351ffe6ff2d4a3c75
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-12-14 11:44:54 +0530

    Added secrets

---
+++ src/secrets.js
@@ -0,0 +1,10 @@
+const secrets={
+    apiKey: "AIzaSyD6UQe52mTLj4waKlXHS6VNKdzWG1bjelk",
+    authDomain: "busybuy-36db2.firebaseapp.com",
+    projectId: "busybuy-36db2",
+    storageBucket: "busybuy-36db2.appspot.com",
+    messagingSenderId: "990632854760",
+    appId: "1:990632854760:web:47e6e5e988fe264716755d"
+  };
+
+  export default secrets;
